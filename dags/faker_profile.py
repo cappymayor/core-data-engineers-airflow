721639b3de1596015b8171fa83b099866948ca28
Mayowa Akinyele <mayowaakinyele@Mayowa.Akinyele MacBookPro>
2024-02-08 19:30:29 +0100

    linting

--- dags/faker_profile.py
+++ dags/faker_profile.py
@@ -6,15 +6,10 @@ from utils.fakers_operations import profile_data_generator
 def data_extract_to_s3():
 
     wr.s3.to_parquet(
-                    df = profile_data_generator(200),
-                    path = "s3://faker-raw-data/user_profile/",
-                    boto3_session = aws_sesion(),
-                    mode = "append",  
+                    df=profile_data_generator(200),
+                    path="s3://faker-raw-data/user_profile/",
+                    boto3_session=aws_sesion(),
+                    mode="append",
                     dataset=True
                     )
     return "Data successfully written to s3"
-
-
-
-
-
